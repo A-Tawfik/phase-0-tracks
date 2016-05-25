b7a86e68c389fd361585e17dcdfde8300aeef253
A-Tawfik <taamer@gmail.com>
2016-05-24 22:33:20 -0400

    add data display in sentence

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -3,17 +3,28 @@ puts "Hamsters name?"
 h_name = gets.chomp
 #ask volume
 puts "hamster volume? (1-10)"
-volume = gets.chomp
+volume = gets.to_i
 #ask fur color
 puts "hamster fur color?"
 h_color = gets.chomp
 #good or bad for adoption
 puts "Hamster good for adoption? (y/n)"
-adopt = gets.chomp
+adopt = gets[0] == 'y'
 #estimated age
 puts "Estimated age?"
-age = gets.chomp
+age = gets.to_i
 
-if age == ""
+if age == 0
   age = nil
 end
+
+if adopt
+  adopt = "is"
+else
+  adopt = "is not"
+end
+
+puts "#{h_name} is #{age} years old and has #{h_color} fur.
+#{h_name} has an estimated volume level of #{volume} and #{adopt} ready for adoption" 
+
+p age
