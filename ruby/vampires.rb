02c89111d688764b3b08ea554af737c8bf5fef21
A-Tawfik <taamer@gmail.com>
2016-05-26 17:52:44 -0400

    add if statment for vampire checking

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -2,14 +2,26 @@
 puts "Whats your name?"
 name = gets.chomp
 #ask age
-puts "How old are you?"
-age = gets.chomp
-#ask birth_place
-puts "Where were you born?"
-birth_place = gets.chomp
+puts "How old will you be by the end of this year?"
+age = gets.chomp.to_i
+#ask birth_year
+puts "What year were you born?"
+birth_year = gets.chomp.to_i
 #Ask garlic bread
 puts "Would you like some garlic bread?(y/n)"
 garlic_ok = gets.chomp
 #ask Health insurance
 puts "Would you like to enroll in the company's health insurance?(y/n)"
-insurance = gets.chomp
+insurance_ok = gets.chomp
+
+if (age == 2016 - birth_year) && ((garlic_ok == "y") || (insurance_ok == "y"))
+  puts "Probably not a vampire."
+elsif !(age == 2016 - birth_year) && (garlic_ok != "y") && (insurance_ok != "y")
+  puts "Almost certainly a vampire."
+elsif !(age == 2016 - birth_year) && (garlic_ok != "y") || (insurance_ok != "y")
+  puts "Probably a vampire."
+elsif name == "Drake Cula" || name == "Tu Fang"
+  puts "Definitely a vampire."
+else
+  puts "Result inconclusive."
+end
