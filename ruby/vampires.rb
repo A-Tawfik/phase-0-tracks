efd23e9888082047cf555e6cda66b801e5242c49
A-Tawfik <taamer@gmail.com>
2016-05-26 02:35:22 -0400

    add questions to gather info

---
+++ ruby/vampires.rb
@@ -0,0 +1,15 @@
+#ask name
+puts "Whats your name?"
+name = gets.chomp
+#ask age
+puts "How old are you?"
+age = gets.chomp
+#ask birth_place
+puts "Where were you born?"
+birth_place = gets.chomp
+#Ask garlic bread
+puts "Would you like some garlic bread?(y/n)"
+garlic_ok = gets.chomp
+#ask Health insurance
+puts "Would you like to enroll in the company's health insurance?(y/n)"
+insurance = gets.chomp
