c08ac4db46380219cc6fe392640fb92c72253747
A-Tawfik <taamer@gmail.com>
2016-05-26 22:44:34 -0400

    add decrypt and conditional edge case

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -1,34 +1,61 @@
-#get string input
+#Get string input
   puts "write a thing"  
   password = gets.chomp
 
 #encryption method
 def encrypt(str)
-  #define loop variables
+  #define loop variables 
   times = 0
   encrypted = ""
-  #loop 'string length' number of times
+  #loop "string length" number of times
   until times == str.length
-    #add next letter to encrypted string
-    encrypted += str[times].next
+    current_letter = str[times]
+    #conditional edge cases and space
+    if current_letter == "z"
+      current_letter = "a"
+    elsif current_letter == " "
+      current_letter = " "
+    else
+      #find next letter
+      current_letter = str[times].next
+    end
+    #add letters to string one by one
+    encrypted += current_letter
+    
     times += 1
   end
   return encrypted
 end
-
-#print result string
+#print
 puts encrypt(password)
 
+
+##DECRYPTION##
+#get user input string
   puts "write a thing"  
   password = gets.chomp
-
+#decryption method
 def decrypt(str)
+  #define loop variable
   times = -(str.length)
   decrypted = ""
-  
+  #loop "string length" number of times
   until times == 0
-    decrypted += (str[times].ord-1).chr
+    current_letter = str[times]
+    #conditional edge cases and space
+    if current_letter == "a"
+      current_letter = "z"
+    elsif current_letter == " "
+      current_letter = " "
+    else
+      #find previous letter 
+      current_letter = (str[times].ord-1).chr
+    end
+    #add letters to string one by one
+    decrypted += current_letter
     times += 1
   end
-  
+  return decrypted
+end
+ #print string
   puts decrypt(password)
