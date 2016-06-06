d6c977b18352609e1892773639b74e560c485c24
A-Tawfik <taamer@gmail.com>
2016-06-06 07:09:03 -0400

    add puppy characteristics and behavior

---
+++ ruby/puppy.rb
@@ -0,0 +1,16 @@
+# SPECIES -------------------
+# Caninicus Largeheadicus
+
+# CHARACTERISTICS -----------
+# Head to body ratio: 1/2
+# Friendly: extreamly
+# Legs: 4
+# Paw Size: large for body
+# Color Varies
+
+# BEHAVIOR-------------------
+# play
+# fetch
+# sleep
+# eat
+# bound
