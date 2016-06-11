446e27369cd85a94918196bb001271acdb2beea8
A-Tawfik <taamer@gmail.com>
2016-06-10 22:04:42 -0400

    calculator

--- ruby/calculator/calculator_spec.rb
+++ ruby/calculator/calculator_spec.rb
@@ -2,6 +2,7 @@
 # add two integers and return an integer
 # subtract two integers and return an integer
 # multiply two integers and return an integer
+# divide two integers and return and integer
 
 # RSpec
 # 'describe' block for each group of tests  
@@ -24,4 +25,9 @@ describe Calculator do
   it "multiplies two integers" do
     expect(calculator.multiply(2,3)).to eq 6
   end
+
+  it "divides two integers" do
+    expect(calculator.divide(16,4)).to eq 4
+  end
+  
 end
