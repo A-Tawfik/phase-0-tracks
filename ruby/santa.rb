f2f47c3255c249800944ce59727428645dea63d9
A-Tawfik <taamer@gmail.com>
2016-06-10 21:26:18 -0400

    Add driver code for santas array

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,14 +1,17 @@
 class Santa
 
-  def initialize(gender, ethnicity)
+  def initialize(gender, ethnicity, painted_face)
     puts "Initializing Santa instance..."
-    @name = name
-    @painted_face = painted_face
+    @painted_face = painted_face == "painted"
     @gender = gender
     @ethnicity = ethnicity
-    @reindeer_ranking = 
+    @reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
     @age = 0
   end  
+
+  def age(age)
+    @age = age
+  end
 
   def reindeer_rank(reindeer_ranking)
     @reindeer_ranking = reindeer_ranking
@@ -27,6 +30,8 @@ class Santa
     puts "This santa is #{@gender} and #{@ethnicity}"
   end
 
+  
+
 
 end
 
@@ -34,8 +39,8 @@ end
 def random_reindeer_list
   all_reindeer = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
   new_reindeer = []
-  while new_reindeer.length < 9
-    random_number = rand(9)
+  while new_reindeer.length < all_reindeer.length
+    random_number = rand(all_reindeer.length)
     if !new_reindeer.include?(all_reindeer[random_number])
       new_reindeer << all_reindeer[random_number]
     end
@@ -45,20 +50,32 @@ end
 
 #Random Gender Selector
 def random_gender
-  facebook_genders = ["Agender", "Androgyne", "Androgynous", "Bigender", "Cis", "Cisgender", "Cis Female", "Cis Male", "Cis Man", "Cis Woman", "Cisgender Female", "Cisgender Male", "Cisgender Man", "Cisgender Woman", "Female to Male", "FTM", "Gender Fluid", "Gender Nonconforming", "Gender Questioning", "Gender Variant", "Genderqueer", "Intersex", "Male to Female", "MTF", "Neither", "Neutrois", "Non-binary", "Other", "Pangender", "Trans", "Trans*", "Trans Female", "Trans* Female", "Trans Male", "Trans* Male", "Trans Man", "Trans* Man", "Trans Person", "Trans* Person", "Trans Woman", "Trans* Woman", "Transfeminine", "Transgender", "Transgender Female", "Transgender Male", "Transgender Man", "Transgender Person", "Transgender Woman", "Transmasculine", "Transsexual", "Transsexual Female", "Transsexual Male", "Transsexual Man", "Transsexual Person", "Transsexual Woman", "Two-Spirit"]
+  facebook_genders = ["Agender", "Androgyne", "Androgynous", "Bigender", "Cis", "Cisgender", "Cis Female", "Cis Male", "Cis Man", "Cis Woman", "Cisgender Female", "Cisgender Male", "Cisgender Man", "Cisgender Woman", "Female to Male", "FTM", "Gender Fluid", "Gender Nonconforming", "Gender Questioning", "Gender Variant", "Genderqueer", "Intersex", "Male to Female", "MTF", "Neither", "Neutrois", "Non-binary", "Other", "Pangender", "Trans", "Trans*", "Trans Female", "Trans* Female", "Trans Male", "Trans* Male", "Trans Man", "Trans* Man", "Trans* Person", "Trans Person", "Trans* Woman", "Trans Woman", "Transfeminine", "Transgender", "Transgender Female", "Transgender Male", "Transgender Man", "Transgender Person", "Transgender Woman", "Transmasculine", "Transsexual", "Transsexual Female", "Transsexual Male", "Transsexual Man", "Transsexual Person", "Transsexual Woman", "Two-Spirit"]
   gender = facebook_genders[rand(facebook_genders.length)]
 end
 
+# Random Ethnicity selector
 def random_ethnicity
   all_ethnicity_arr = ["Afghan","Albanian","Arab","Argentine","American musicians of Armenian descent","Armenian","Australian","Austrian","Azerbaijani","Bahamian","Bangladeshi","Barbadian","Belgian","Brazilian","Bulgarian","Cajuns","Cambodian","Chinese","Colombian","Coptic","Croatian","Cuban","Czech","Danish","Dominican","Dutch","Egyptian of English descent","Estonian","Ethiopian","Finnish","French","German","German Texans","Greek","Guyanese","Haitian","Hispanic/Latino","Hmong","Honduran","Hungarian","Indian","indigenous people of the Americas","Indonesian","American Inuit","Iranian","Iraqi","Irish","Israeli","Italian","Jamaican","Japanese","Kazakh","Korean","Laotian","Lebanese","Louisiana Creoles","Macedonian","Mexican","User:BlindForay/sandbox","Native Hawaiians","Nicaraguan","Nigerian","Norwegian","Pakistani","Palestinian","Panamanian","Polish","Portuguese","Stateside Puerto Ricans","Romanian","Russian","Rusyn","Salvadoran","Scotch-Irish","Scottish","Serbian","Sicilian","Slovak","Slovene","Somali","Spanish","Sri Lankan","Swedish","Swiss","Taiwanese","Turkish","Ukrainian","Uruguayan","Venezuelan","Vietnamese","Welsh"]
   ethnicity = all_ethnicity_arr[rand(all_ethnicity_arr.length)]
 end
 
-p random_reindeer_list
-p random_ethnicity
-p random_gender
+
+#creates 100 unique santas (1.8 billion possibilities!!!! MUAHAHAHAHA!)
+santas = []
+ 100.times do |i|
+  if rand(100) > 53 
+    face = "painted"
+  else
+    face = "Not painted" 
+  end
+  santas << Santa.new(random_gender, random_ethnicity, face)
+end
+santas.each {|santa| santa.reindeer_rank(random_reindeer_list)}
+santas.each {|santa| santa.age(rand(20..100))}
 
 
+p santas
 
 
 
