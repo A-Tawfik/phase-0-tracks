90d0f2f0d47107c267eb49cea733e05938b9be95
A-Tawfik <taamer@gmail.com>
2016-06-11 20:03:14 -0400

    add 2 classes that include module and its methods

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,12 +1,37 @@
+# module Shout
+#   def self.yell_angrily(words)
+#     words + "!!!!" + ":("
+#   end
+# 
+#   def self.yell_happily(words)
+#     words + "!" + ":D"
+#   end
+# end
+# 
+# puts Shout.yell_happily("I love pie")
+# puts Shout.yell_angrily("i HATE pie")
+
 module Shout
-  def self.yell_angrily(words)
-    words + "!!!!" + ":("
+  def yell_angrily(words)
+    words + "!!!!!" + ":["
   end
 
-  def self.yell_happily(words)
-    words + "!" + ":D"
+  def yell_happily(words)
+    words + "!" + "YIPPY!" + ":D"
   end
 end
 
-puts Shout.yell_happily("I love pie")
-puts Shout.yell_angrily("i HATE pie")
+class Girlfriend
+  include Shout
+end
+
+class Pirate
+  include Shout
+end
+
+black_beard = Pirate.new
+puts black_beard.yell_angrily("ARRRRR")
+
+gf = Girlfriend.new
+puts gf.yell_happily("You got me cookies?")
+
