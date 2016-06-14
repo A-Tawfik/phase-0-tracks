934ea28d3a252065a90ddb225f654489d8ecaf8a
A-Tawfik <taamer@gmail.com>
2016-06-14 01:14:21 -0400

    write pseudocode for reverse string function

---
+++ js/explore.js
@@ -0,0 +1,8 @@
+// break up the string by its individual characters using a for loop
+// save first letter into a 'new string' to be built on
+// for each iteration of the loop, add the current letter plus the 'new string'
+//lather, rinse, repeat
+
+for (var i = 0; i < str.length; i++) {
+  console.log(str[i]);
+}
