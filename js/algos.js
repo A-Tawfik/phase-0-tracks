a582521670612aa39c54425d1f498901e68c15b4
A-Tawfik <taamer@gmail.com>
2016-06-18 16:13:16 -0600

    add pseudocode for longest string function

---
+++ js/algos.js
@@ -0,0 +1,8 @@
+// function that takes and array of words or phrases and 
+// returns the longest in the array
+  // function receives array as parameter
+  // iterate through each input to find the length of each string
+  // within the iteration, compare next iteration to saved longest (current_string, longest_string variables)
+  // return longest_string
+
+  
