b2168377a6eb7b667470d024cbaec0a3ab45ab8b
A-Tawfik <taamer@gmail.com>
2016-06-18 16:34:52 -0600

    add function with driver code for finding the longest phrase in array

--- js/algos.js
+++ js/algos.js
@@ -5,4 +5,17 @@
   // within the iteration, compare next iteration to saved longest (current_string, longest_string variables)
   // return longest_string
 
-  
+function longestPhrase(phrases) {
+  this.phrases = phrases;
+  longestString = ""
+
+  for (var i=0; i < phrases.length; i++) {
+    if (phrases[i].length > longestString.length) {
+      longestString = phrases[i];
+    }
+  }
+  return longestString;
+}
+
+var words = ["This is the longest by far", "apples", "a BANANA"];
+console.log(longestPhrase(words));
